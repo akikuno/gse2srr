e3818d82a5a02735e1c12d64dd722df0ce7332e7
Akihiro Kuno <akikuno@users.noreply.github.com>
2024-08-01 17:22:02 +0900

    Support both wget and curl

--- gse2srr.sh
+++ gse2srr.sh
@@ -2,11 +2,24 @@
 
 GSE="$1"
 
-wget -qO - https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc="$GSE" |
+if [ -z "$GSE" ]; then
+    echo "Error: Please provide a GSE accession id"
+    exit 1
+fi
+
+if type curl >/dev/null 2>&1; then
+    CMD_CURL='curl -s'
+elif type wget    >/dev/null 2>&1; then
+    CMD_CURL='wget -qO -'
+else
+    error_exit 1 'No HTTP-GET/POST command found.'
+fi
+
+$CMD_CURL https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc="$GSE" |
     grep "/soft/" |
     cut -d '"' -f 2 |
     sed "s|$|${GSE}_family.soft.gz|" |
-    xargs wget -qO - |
+    xargs $CMD_CURL |
     gzip -dc |
     grep SRX |
     awk '{print "wget -qO -", $NF, " | tr \">\" \"\\n\" | grep \"^SRR\" | cut -d \"<\" -f 1 &"} END {print "wait"}' |
